033c26ee43a181f6548b0bb41f8f9463486070c4
MARVIN <o.silverygold@gmail.com>
2023-05-15 14:11:44 +0300

    Add JavaScript for Dynamic HTML

---
+++ main.js
@@ -0,0 +1,33 @@
+let cont = document.querySelector('#dynamic-container');
+
+function addElement() {
+    let el = document.createElement('button');
+    el.id = 'created';
+    el.className = 'created-element';
+    el.textContent = 'Created';
+
+    cont.appendChild(el);
+}
+
+function alterElement(){
+    let altered_el = document.querySelector('#created');
+    altered_el.style.background = 'green';
+}
+
+function alterAllElements(){
+    let altered_el = document.querySelectorAll('#created');
+    
+    altered_el.forEach(elem =>{
+        elem.style.background = 'yellow';
+        elem.style.color = 'black';
+    })
+}
+
+function deleteElements(){
+    let alldynamicElements = document.querySelectorAll('#created');
+
+    alldynamicElements.forEach(elem => {
+        elem.style.display = 'none';
+    })
+}
+
